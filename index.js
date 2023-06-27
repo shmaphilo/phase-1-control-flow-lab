8c3f2a726652d81ea800e3bdc60fe6738091a33d
shmaphilo <philipogaye062@gmail.com>
2023-06-27 13:09:58 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+
+  if(ride <=400){
+    return "This one is on me!"
+
+  } else if(ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks."
+
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// console.log(scuberGreetingForFeet(2600));
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// console.log(ternaryCheckCity('N'));
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+     case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."; 
+  }
 }
+
+  
